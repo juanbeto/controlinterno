c346e0f5685ccb1414f5c3c0012341444742d5fc
juanbeto <juank01@gmail.com>
2019-01-14 23:15:00 +0000

    Ene 14

---
+++ app/Helpers/JwtAuth.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Helpers;
+use Firebug\JWT\JWT;
+use Illuminate\Support\Facades\DB;
+use App\AdminUser;
+
+class JwtAuth{
+  private $key;
+  private $encript_algoritm
+
+  funtion __construct(){
+      $this->key = 'HJSKaquef_*12A89p';
+      $this->encript_algoritm = 'HS256';
+  }
+
+  public function signup($email, $password, $getToken=null){
+
+      $signup = false;
+
+      $user = USER::where(
+        array(
+          'email'=>$email,
+          'password'=>$password
+        )
+      )->firts();
+
+
+      if(is_object($user)){
+        $signup = true;
+      }
+
+      if($signup){
+        //Generar token
+        $token = array(
+          'sub'   =>  $user->id,
+          'email' =>  $user->email,
+          'name'  =>  $user->name,
+          'iat'   =>  time(),
+          'exp'   =>  time() + (60*60)//Una hora de session valida
+        );
+
+        $jwt = JWT::encode($token, $this->key, $this->encript_algoritm);
+        $decode = JWT::decode($jwt, $this->key, array($this->encript_algoritm));
+        if(!is_null($getToken))
+        {
+          return $jwt;
+        }else{
+          return $decode;
+        }
+
+      }else{
+        //devolver date_get_last_errors
+        return array('status' => 'error', 'message' => 'Login ha fallado!!' );
+      }
+  }
+
+  public function checkToken($jwt, $getIdentity = false){
+    $auth = false;
+    try{
+      $decode = JWT::decode($jwt, $this->key, array($this->encript_algoritm));
+
+    }catch(\UnexpectedValueException $e){
+      $auth = false;
+    }catch(\DomainException $e){
+      $auth = false;
+    }
+
+    if(is_object($decode) && isset($decode->sub)){
+      $auth = true;
+    }else{
+      $auth = false;
+    }
+
+    if($getIdentity){
+      return $decode;
+    }
+
+    return $auth;
+  }
+}
+?>
